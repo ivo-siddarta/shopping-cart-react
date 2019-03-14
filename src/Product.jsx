cca99c0504e846572afec364d182cf8166ade788
Ivo Siddarta <ivo.siddarta@berkeley.edu>
2019-03-14 14:48:33 -0700

    added Product.jsx component to render multiple products

---
+++ src/Product.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Product = props => {
+  let counter = 0;
+  function addToCart(e) {
+    counter += 1;
+    if (counter > props.limit) {
+      alert("This item is out of stock!");
+    } else {
+      alert(
+        "There are " + counter + " " + props.productName + "s in your cart!"
+      );
+    }
+  }
+  return (
+    <div class="ui cards">
+      <div class="card">
+        <div class="content">
+          <div class="header">{props.productName}</div>
+          <div class="description" />
+          Price: ${props.price}
+        </div>
+        <div class="ui bottom attached button" onClick={addToCart}>
+          <i class="add icon" />
+          Add to Cart
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
